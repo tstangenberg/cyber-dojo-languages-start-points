792468b360a26d7dda39fd19a20fb6a1e279510f
Jon Jagger <jon@jaggersoft.com>
2020-06-03 13:24:39 +0100

    [ci skip] notes update

--- docs/tagging-the-ltf-images.sh
+++ docs/tagging-the-ltf-images.sh
@@ -2,6 +2,8 @@
 
 # $ cyber-dojo start-point build --language=<name> <url>...
 # Work in Progress.
+# See start-points-base  branch=build-option
+# See commander          branch=build-option
 
 # NB: context dir is docker volume mounted (so not /tmp)
 CONTEXT_DIR=/Users/jonjagger/tmp-start-point-build
@@ -18,24 +20,26 @@ git clone "${LTF_GIT_URL}" "${URL_INDEX}"
 LTF_DIR="${CONTEXT_DIR}/${URL_INDEX}"
 IMAGE_NAME=$(docker run --rm --volume ${LTF_DIR}:/data:ro cyberdojofoundation/image_namer)
 
-# 3. get the commit SHA from the image's env-var
+# 3. ensure we have the latest
 docker pull ${IMAGE_NAME}
+
+# 4. get the commit SHA from the image's env-var
 SHA_ENV_VAR=$(docker run --rm "${IMAGE_NAME}" bash -c 'env | grep SHA=')
 SHA="${SHA_ENV_VAR:4}"
 TAG="${SHA:0:7}"
 
-# 4. ensure the tagged image exists
+# 5. ensure the tagged image exists
 docker pull ${IMAGE_NAME}:${TAG}
 
-# 5. checkout the start_point's commit SHA
+# 6. checkout the start_point's commit SHA
 cd "${LTF_DIR}"
 git reset --hard HEAD
 git checkout "${SHA}" # detached head warning
 
-# 6. remove unwanted files
+# 7. remove unwanted files
 rm -rf docker
 rm -rf .git
 
-# 7. Save SHA info so start-point service can append
+# 8. Save SHA info so http start-point service can append
 #    the TAG to image_name when serving manifests.
-echo -e "${URL_INDEX} \t ${SHA} \t ${LTF_GIT_URL}" >> ${CONTEXT_DIR}/shas.txt
+echo -e "${URL_INDEX} \t ${SHA} \t ${LTF_GIT_URL}" >> ${CONTEXT_DIR}/build_shas
