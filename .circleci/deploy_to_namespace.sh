2e2defbb91a70ba4b45fa803bfd63e3cd6b1a940
Jon Jagger <jon@jaggersoft.com>
2020-07-10 12:28:39 +0100

    Drop all/common/small distinction in k8s deployment script

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -5,7 +5,6 @@ readonly K8S_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master
 readonly VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
 source <(curl "${K8S_URL}/sh/deployment_functions.sh")
 export $(curl "${VERSIONER_URL}/app/.env")
-readonly CYBER_DOJO_LANGUAGES_START_POINTS_IMAGE=cyberdojo/languages-start-points-all  # special value
 readonly CYBER_DOJO_LANGUAGES_START_POINTS_TAG="${CIRCLE_SHA1:0:7}"
 
 gcloud_init
